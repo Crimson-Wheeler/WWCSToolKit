de3e28cf05890ce87e5b84160e7e4159ab0934a6
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2021-11-03 10:01:41 -0400

    Update WWCS_NetworkManagement.psm1

--- WWCS_NetworkManagement.psm1
+++ WWCS_NetworkManagement.psm1
@@ -1,9 +1,15 @@
 ﻿function Get-NetworkInfo
 {
+
     $fileName = "NetworkInfo.txt"
     $directory = "C:\Temp\Logs"
     $filePath = "$($directory)\$($fileName)"
-    if(-not (Test-Path -Path "C:\Temp\Logs"))
+
+    if(Test-Path -Path $filePath)
+    {
+        Remove-Item $directory    
+    }
+    if(-not (Test-Path -Path $directory))
     {
         New-Item $directory -ItemType Directory
     }
