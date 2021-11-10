39d78df5c7a804901d6b633e51a4b0a3903f757e
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2021-11-10 14:43:55 -0500

    Update WWCS_Programs.psm1

--- WWCS_Programs.psm1
+++ WWCS_Programs.psm1
@@ -1,9 +1,21 @@
 ﻿function Install-BillingReconciliation()
 {
-
+    
 }
 
 function Open-Program($path)
 {
     Start-Process $path
+}
+function Open-WWCSProgram($programName)
+{
+    if($programName -eq $null)
+    {
+    
+    }
+    else
+    {
+        Start-Process "$(Get-WWCSTOOLKITPath)\$programName"
+    }
+    
 }
