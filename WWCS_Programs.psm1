37e01db737f6f357aa61a08dd313ef2c11f3610b
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2021-11-10 15:54:45 -0500

    Update WWCS_Programs.psm1

--- WWCS_Programs.psm1
+++ WWCS_Programs.psm1
@@ -17,7 +17,34 @@ function Open-WWCSProgram($programName)
 {
     if($programName -eq $null)
     {
-    
+        #list all files in the programs
+        #select a number from them
+        #start that process
+
+        [string]$path = Get-WWCSProgramPath
+
+        $files = [System.Collections.ArrayList]@()
+        $files.AddRange(@(Get-ChildItem $($path)));
+
+        Write-Output " "
+        Write-Output " "
+        Write-Output " "
+        Write-Output "Program Num | Program Choice to Run"
+        Write-Output "-------------------------------------------"
+        [int]$index = 1
+        foreach($item in $files)
+        {
+            Write-Output "$($index)          | $($files[($index-1)])"
+            $index++
+        }
+        Write-Output " "
+        Write-Output " "
+        Write-Output " "
+        [int]$choice =  Read-Host -Prompt 'Pick your File to set to your clipboard'
+        [string]$val = "$($path)\$($files[($choice-1)])"
+        Write-Host $val
+        Start-Process $val
+
     }
     else
     {
