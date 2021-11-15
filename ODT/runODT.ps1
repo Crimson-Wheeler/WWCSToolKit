3ed79e44f4e967f46fc31a312dd3859c3a7e7fdc
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2021-11-15 14:52:57 -0500

    Update runODT.ps1

--- ODT/runODT.ps1
+++ ODT/runODT.ps1
@@ -1,1 +1,5 @@
-﻿setup.exe \configure Business_32_Config.xm
+﻿#setup.exe \configure Business_32_Config.xm
+
+$path = "$(Get-WWCSTOOLKITPath)\ODT\setup.exe"
+$arguments = "\configure Business_32_Config.xml"
+Start-Process -FilePath $path  $arguments
