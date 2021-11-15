81d0bcab02fb2853fccab83cf271117ad632de8d
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2021-11-15 15:23:46 -0500

    Update runODT.ps1

--- ODT/runODT.ps1
+++ ODT/runODT.ps1
@@ -1,5 +1,9 @@
 ﻿#setup.exe \configure Business_32_Config.xm
 
-$path = "$(Get-WWCSTOOLKITPath)\ODT\setup.exe"
+#$path = "$(Get-WWCSTOOLKITPath)\ODT\setup.exe"
 $arguments = "\configure Business_32_Config.xml"
-Start-Process -FilePath $path  $arguments
+#Start-Process $path -ArgumentList $arguments
+#Start-Sleep -Seconds 10
+
+cd "$(Get-WWCSTOOLKITPath)\ODT"
+Start-Process "setup.exe" -ArgumentList $arguments
