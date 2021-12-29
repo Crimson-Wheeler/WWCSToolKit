903f9de22489f7ebc62fbb724276bd66ecd815aa
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2021-12-29 10:40:30 -0500

    Update WWCS_SoftwareManagement.psm1

--- WWCS_SoftwareManagement.psm1
+++ WWCS_SoftwareManagement.psm1
@@ -6,10 +6,9 @@ function Test-AppInstalled($appName)
     [bool]$onProgramFiles86 = $false
     [bool]$onAppData = $false
 
-    #Testing for Local Machine
     $INSTALLED = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, UninstallString
     $INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, UninstallString
-    #$INSTALLED | ?{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | Format-Table -AutoSize
+
     $SEARCH = $appName
     $RESULT =$INSTALLED | ?{ $_.DisplayName -ne $null } | Where-Object {$_.DisplayName -like "*$appName*" } 
     $RESULT # this is the application object
@@ -17,15 +16,7 @@ function Test-AppInstalled($appName)
     {
         $onHKLM = $true
     }
-    #End local machine
 
-    #Testing for WMI
-    <#-$MyApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -like "*$appName*"}
-    if($MyApp -ne $null)
-    {
-        $onWMI = $true
-    }
-    #End WMI-#>
 
     $folders = Get-ChildItem -Path "C:\Program Files" *$appName* -Recurse -Depth 1
     if($folders.Count -gt 0)
@@ -38,16 +29,16 @@ function Test-AppInstalled($appName)
         $onProgramFiles86 = $true
     }
 
-    $children = Get-ChildItem -path "C:\users" Local -Recurse -Depth 3 -Force
+
+    
+    $children = Get-ChildItem -path "C:\users" Local -Recurse -Depth 3 -Force -ErrorAction SilentlyContinue
     for (($i = 0); $i -lt $children.Count; $i++)
     {
-        
         try {
             $child = $children[$i]
             $path = $child.ToString()
-            Write-Host $child.FullName
             Write-Host $child.ToString()
-            $app = Get-ChildItem $path #*$appName*
+            $app = Get-ChildItem $path *$appName*
             
             if($app -ne $null)
             {
@@ -60,28 +51,6 @@ function Test-AppInstalled($appName)
         }
        
     }
-
-    <#
-    foreach($child in Get-ChildItem -path "C:\users" Local -Recurse -Depth 3 -Force)
-    {
-        try {
-            $path = $child.ToString()
-            Write-Host $child.FullName
-            Write-Host $child.ToString()
-            $app = Get-ChildItem $path #*$appName*
-            
-            if($app -ne $null)
-            {
-                Write-Host "Application Installed under:"$child.Parent.Parent.Name
-                $onAppData = $true
-            }
-        }
-        catch {
-            
-        }
-    } 
-    #>
-
 
     #Get a list through app and features
     #Get-ControlPanelItem "Programs and Features"     
