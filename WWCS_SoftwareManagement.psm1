ce0eb7472fd9c7c20baf950b2de50500ce80a1ff
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2021-12-29 10:48:40 -0500

    Update WWCS_SoftwareManagement.psm1

--- WWCS_SoftwareManagement.psm1
+++ WWCS_SoftwareManagement.psm1
@@ -6,9 +6,10 @@ function Test-AppInstalled($appName)
     [bool]$onProgramFiles86 = $false
     [bool]$onAppData = $false
 
+    Write-Host "Future implementation of WMI here..."
+    Write-Host "Testing HKLM..."
     $INSTALLED = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, UninstallString
     $INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, UninstallString
-
     $SEARCH = $appName
     $RESULT =$INSTALLED | ?{ $_.DisplayName -ne $null } | Where-Object {$_.DisplayName -like "*$appName*" } 
     $RESULT # this is the application object
@@ -17,12 +18,14 @@ function Test-AppInstalled($appName)
         $onHKLM = $true
     }
 
-
+    Write-Host "Testing Program Files..."
     $folders = Get-ChildItem -Path "C:\Program Files" *$appName* -Recurse -Depth 1
     if($folders.Count -gt 0)
     {
         $onProgramFiles = $true
     }
+
+    Write-Host "Testing Program Files x86..."
     $folders = Get-ChildItem -Path "C:\Program Files (x86)" *$appName* -Recurse -Depth 1
     if($folders.Count -gt 0)
     {
@@ -30,7 +33,7 @@ function Test-AppInstalled($appName)
     }
 
 
-    
+    Write-Host "Testing App Data installs..."
     $children = Get-ChildItem -path "C:\users" Local -Recurse -Depth 3 -Force -ErrorAction SilentlyContinue
     for (($i = 0); $i -lt $children.Count; $i++)
     {
@@ -54,12 +57,12 @@ function Test-AppInstalled($appName)
 
     #Get a list through app and features
     #Get-ControlPanelItem "Programs and Features"     
-
-    Write-Host "Install found with WMI:                 "$onWMI
-    Write-Host "Install found with HKLM:                "$onHKLM
-    Write-Host "Install found with Program Files:       "$onProgramFiles
-    Write-Host "Install found with Program Files x86:   "$onProgramFiles86
-    Write-Host "Install found with App Data:            "$onAppData
+    Write-Host "Informational Output...................."
+    Write-Host "Install found with WMI.................."$onWMI
+    Write-Host "Install found with HKLM................."$onHKLM
+    Write-Host "Install found with Program Files........"$onProgramFiles
+    Write-Host "Install found with Program Files x86...."$onProgramFiles86
+    Write-Host "Install found with App Data............."$onAppData
     if($onHKLM -or $onWMI -or $onProgramFiles -or $onProgramFiles86 -or $onAppData)
     {
         return $true
