4103f866c03af3240307ca2246d8c7eb899548ec
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2022-01-26 09:49:29 -0500

    Update WWCS_DirectoryManagement.psm1

--- WWCS_DirectoryManagement.psm1
+++ WWCS_DirectoryManagement.psm1
@@ -1,6 +1,5 @@
 function Get-DirectoryPermissions($directory, $exportLocation = "C:\Temp", [switch]$findDir, [switch]$Recurse) {
     $exportFile = "$exportLocation\folderPermissions.csv"
-
     if($findDir){$directory = Get-Directory}
     if(-not $directory){$directory = Get-Directory}
 
@@ -15,12 +14,6 @@ function Get-DirectoryPermissions($directory, $exportLocation = "C:\Temp", [swit
     }
 
     $dir = Get-Item -Path $directory
-    
-    #look at the permissions
-    #foreach user who has permissions, dir, username, permission type
-    #WRITE CUSTOM OBJECT WITH PERMISSION INFO
-
-
     $permissionObjects = (Get-Acl -Path $dir).Access
     foreach ($obj in $permissionObjects) {
         if($obj.PropagationFlags -ne 'None'){continue}
@@ -31,7 +24,6 @@ function Get-DirectoryPermissions($directory, $exportLocation = "C:\Temp", [swit
             Permissions = $obj.FileSystemRights
             
         }
-        Write-Host $folderInfo
         Export-Csv -InputObject $folderInfo -Path $exportFile -Append
     }
 }
