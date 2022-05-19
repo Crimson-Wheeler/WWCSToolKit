eb3509ffce82ce332429feb9d324de44bad209fd
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2022-05-19 08:44:58 -0400

    Update Installer

--- Ignored Scripts/WWCSToolkit-Installer.ps1
+++ Ignored Scripts/WWCSToolkit-Installer.ps1
@@ -1,174 +1,178 @@
-﻿function DownloadGitHubRepository 
-{ 
-    if (Test-Path -Path 'C:\Temp\.zip')
-    {
-        Remove-Item 'C:\Temp\.zip' -Recurse -Force
+﻿$logPath = "C:\Program Files\WWCS\Logs"
+$dataPath = "C:\Program Files\WWCS\DataControl"
+$programPaths = "C:\Program Files\WWCS\Programs"
+$fileName = "WWCS-TOOLKIT.log"
+$tempPath = "C:\Temp"
+$modulePath =  "C:\Windows\system32\WindowsPowerShell\v1.0\Modules"
+$RepositoryZipUrl = "https://github.com/Crimson-Wheeler/WWCSToolkit/archive/main.zip" 
+
+$errors = ""
+try {
+    
+    #region Cleaup 
+    if(Test-Path 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITnew'){
+        Remove-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITnew' -Recurse -Force
     }
-    if (Test-Path -Path 'C:\Temp\WWCSToolKit-main')
-    {
-        Remove-Item  'C:\Temp\WWCSToolKit-main' -Recurse -Force
+    if(Test-Path 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITold'){
+        Remove-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITold' -Recurse -Force
     }
-    if (Test-Path -Path 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCSToolKit-main')
-    {
+    if(Test-Path 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCSToolKit-main'){
         Remove-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCSToolKit-main' -Recurse -Force
     }
-    
-    $logPath = "C:\Program Files\WWCS\Logs"
-    $dataPath = "C:\Program Files\WWCS\DataControl"
-    $programPaths = "C:\Program Files\WWCS\Programs"
-    $fileName = "WWCS-TOOLKIT.log"
-    $tempPath = "C:\Temp"
-    if (Test-Path -Path 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT') {
-        "Path exists!"
-        Remove-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT' -Recurse -Force
+    if(Test-Path 'C:\Temp\WWCSToolKit-main'){
+        Remove-Item  'C:\Temp\WWCSToolKit-main' -Recurse -Force
     }
-    
-     
-    # Force to create a zip file 
-    $ZipFile = "C:\Temp\.zip"
-
-    if(Test-Path -Path "($tempPath)\WWCSToolKit-main")
-    {
-        Remove-Item -Path "($tempPath)\WWCSToolKit-main" -Recurse -Force
+    if(Test-Path 'C:\Temp\wwcstoolkit'){
+        Remove-Item  'C:\Temp\wwcstoolkit' -Recurse -Force
     }
-    if(Test-Path -Path $ZipFile)
-    {
-        Remove-Item -Path $ZipFile -Recurse -Force
+    if(Test-Path 'C:\Temp\.zip'){
+        Remove-Item 'C:\Temp\.zip' -Recurse -Force
     }
+    if(Test-Path 'C:\Temp\wwcstoolkit.zip'){
+        Remove-Item 'C:\Temp\wwcstoolkit.zip' -Recurse -Force
+    }
+    #endregion
 
-
-
-    New-Item $ZipFile -ItemType File -Force
-
-    #$RepositoryZipUrl = "https://github.com/sandroasp/Microsoft-Integration-and-Azure-Stencils-Pack-for-Visio/archive/master.zip"
-    $RepositoryZipUrl = "https://github.com/Crimson-Wheeler/WWCSToolkit/archive/main.zip" 
-    # download the zip 
+    #region downloading 
+    #download the zip 
     Write-Host 'Starting downloading the GitHub Repository'
-    Invoke-RestMethod -Uri $RepositoryZipUrl -OutFile $ZipFile
-    Write-Host 'Download finished'
-    $modulePath =  "C:\Windows\system32\WindowsPowerShell\v1.0\Modules"
-    #Extract Zip File
-    Write-Host 'Starting unzipping the GitHub Repository locally'
+    Invoke-RestMethod -Uri $RepositoryZipUrl -OutFile 'C:\Temp\wwcstoolkit.zip'
 
-    try
+    #extract to toolkit
+    Write-Host 'Extract Folder'
+    if(Test-Path 'C:\Temp\wwcstoolkit.zip')
     {
-        Expand-Archive -Path $ZipFile -DestinationPath $tempPath
-        Expand-Archive -Path $ZipFile -DestinationPath $modulePath
-    }
-    catch
-    {
-        #Add-Type -Path "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.IO.Compression.FileSystem.dll"
-        [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipFile, $tempPath)
-        [System.IO.Compression.ZipFile]::ExtractToDirectory($Zipfile, $modulePath)
-    }
-
-    Write-Host 'Unzip finished'
-    Write-Host 'Compare Module Versions'
-    $tempModule = "$($tempPath)\WWCSToolKit-main"
-    $module = "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCSToolKit-main"
-    $tempVersion = getVersionOfModule("$($tempModule)\WWCS-TOOLKIT.psd1")
-    $moduleVersion = getVersionOfModule("$($module)\WWCS-TOOLKIT.psd1")
-    $module = "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT"
-    if($tempVersion -eq $moduleVersion)
-    {
-        "SUCCESS: WWCS-TOOLKIT is Up to date" > "$($tempPath)\$($fileName)"
-    } 
-    else
-    {
-        "ERROR: Toolkit Out Of Date" > "$($tempPath)\$($fileName)"
-    }
-
-    Write-Host "Renaming to WWCS-TOOLKIT"
-    Rename-Item -Path "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCSToolKit-main" -NewName $module
-     
-    Write-Host "Copy programs to #($progamPaths)"
-    if(Test-Path -Path "$($programPaths)")#removes so everything can be overridden
-    {
-        Remove-Item -Path "$($programPaths)" -Recurse -Force
-    }
-    Copy-Item -Path "$($module)\Programs" -Destination $programPaths -Recurse
-
-    Write-Host "Copy data to #($dataPath)"
-    if(Test-Path -Path "$($dataPath)")#removes so everything can be overridden
-    {
-        Remove-Item -Path "$($dataPath)" -Recurse -Force
-    }
-    Copy-Item -Path "$($module)\DataControl" -Destination $dataPath -Recurse
-
-
-
-
-
-    if(-not (Test-Path -Path "$($logPath)"))
-    {
-        New-Object -TypeName '' -Path $logPath
-    }
-    
-
-
-    
-    
-
-
-
-    Remove-Item -Path $tempModule -Recurse -Force
-    Remove-Item -Path $ZipFile -Force
-}
-
-function getVersionOfModule($DataPath)
-{
-    #$data = Get-Content -Path $DataPath
-    
-    $data = Import-PowerShellDataFile $DataPath
-    $dataInfo = "$($data.Values)".Split(' ')
-    $version = $dataInfo[$dataInfo.Count-1]
-
-    Write-Host "VERSION: $($version)"
-    return $version
-}
-
-function getInstalledVersion()
-{
-    $module = Get-Module "WWCS-TOOLKIT" -ListAvailable
-    return Write-Output $module.Version.ToString()    
-}
-
-
-
-
-try{
-    $psversion = "$(Get-Host | Select-Object Version)"
-
-    if($PSVersionTable.PSVersion.Major -ge 5)
-    {
-        try
-        {
-            #Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser -Force
-            ##Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force
-            #Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope MachinePolicy -Force
-            #Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process -Force
-            #Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope UserPolicy -Force
-            Import-Module Microsoft.PowerShell.Archive
-            DownloadGitHubRepository
-        }
-        catch
-        {
-            foreach($err in $Error)
-            {
-                "ERROR: $($err)" > "C:\Temp\WWCS-TOOLKIT.log"
-            }
-        }
+        Expand-Archive -Path 'C:\Temp\wwcstoolkit.zip' -DestinationPath 'C:\Temp\wwcstoolkit'
     }
     else
     {
-        "ERROR: PowerShell Outdated $($PSVersionTable.PSVersion)" > "C:\Temp\WWCS-TOOLKIT.log"
+        $errors += "\n ERROR: Zip File Failed to Download"
     }
-}
-catch
-{
-    foreach($err in $Error)
+
+
+    if(Test-Path 'C:\Temp\wwcstoolkit\WWCSToolKit-main')
     {
-        Write-Host "ERROR: $($err)"
+        Copy-Item 'C:\Temp\wwcstoolkit\WWCSToolKit-main' -Destination 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITnew' -Recurse -Force
     }
-    Start-Sleep -Seconds 5
+    else
+    {
+        $errors += "\n ERROR: Zip Extraction Failed."
+    }
+    #endregion
+
+    #region Renaming and Deleting old toolkit
+    if(Test-Path -Path "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT")
+    {
+        Rename-Item -Path "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT" `
+                    -NewName "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITold"
+    }
+    if(Test-Path -Path "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITnew")
+    {
+        Rename-Item -Path "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITnew" `
+                    -NewName "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT"
+    }
+    else
+    {
+        $errors += "\n ERROR: Failed to Copy Toolkit to Module Path."
+    }
+    if(Test-Path -Path "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITold")
+    {
+        Remove-Item "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITold" -Recurse -Force
+    }
+    #endregion
+
+    #region Move Programs and Data, Create Directories
+    if(-not(Test-Path "C:\Program Files\WWCS"))
+    {
+        New-Item -Path "C:\Program Files\WWCS" -ItemType "directory"
+    }  
+
+    if(Test-Path "C:\Program Files\WWCS")
+    {
+        if(Test-Path 'C:\Program Files\WWCS\DataControl')
+        {
+            Remove-Item 'C:\Program Files\WWCS\DataControl' -Recurse
+        }
+        Copy-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT\DataControl' `
+                    'C:\Program Files\WWCS\DataControl' -Recurse -Force
+    }  
+    if(Test-Path "C:\Program Files\WWCS")
+    {
+        if(Test-Path 'C:\Program Files\WWCS\Programs')
+        {
+            Remove-Item 'C:\Program Files\WWCS\Programs' -Recurse
+        }
+        Copy-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT\Programs' `
+                    'C:\Program Files\WWCS\Programs' -Recurse -Force
+    }
+    else
+    {
+        $errors += "\n ERROR: WWCS Directory Does not exist."
+    }
+
+    if(Test-Path 'C:\Program Files\WWCS\Programs')
+    {
+        Remove-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT\Programs' -Recurse
+    }
+    else
+    {
+        $errors += "\n ERROR: Programs Folder Failed."
+    }
+    if(Test-Path 'C:\Program Files\WWCS\DataControl')
+    {
+        Remove-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKIT\DataControl' -Recurse
+    }
+    else
+    {
+        $errors += "\n ERROR: Data Control Folder Failed."
+    }
+    #endregion
+
+    #region Cleaup Again
+    if(Test-Path 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITnew'){
+        Remove-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITnew' -Recurse -Force
+    }
+    if(Test-Path 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITold'){
+        Remove-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCS-TOOLKITold' -Recurse -Force
+    }
+    if(Test-Path 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCSToolKit-main'){
+        Remove-Item 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WWCSToolKit-main'
+    }
+    if(Test-Path 'C:\Temp\WWCSToolKit-main'){
+        Remove-Item  'C:\Temp\WWCSToolKit-main' -Recurse -Force
+    }
+    if(Test-Path 'C:\Temp\wwcstoolkit'){
+        Remove-Item  'C:\Temp\wwcstoolkit' -Recurse -Force
+    }
+    if(Test-Path 'C:\Temp\.zip'){
+        Remove-Item 'C:\Temp\.zip' -Recurse -Force
+    }
+    if(Test-Path 'C:\Temp\wwcstoolkit.zip'){
+        Remove-Item 'C:\Temp\wwcstoolkit.zip' -Recurse -Force
+    }
+    #endregion
 }
+catch {
+    foreach ($item in $Error) 
+    {
+        $errors += "\n ERROR: $($item)"
+    }
+}
+
+
+if($errors.Length -gt 0)
+{
+    $errors > "$($tempPath)\$($fileName)"
+}
+else 
+{
+    "SUCCESS: WWCS-TOOLKIT is Up to date" > "$($tempPath)\$($fileName)"
+}
+
+
+
+
+
+
+
+
