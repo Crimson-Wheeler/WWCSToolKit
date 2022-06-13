e54e42eadbfd1d923df9eeedcd37a2afd8e0dace
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2022-06-13 16:53:35 -0400

    Update WWCS_PrinterManagement.psm1

--- WWCS_PrinterManagement.psm1
+++ WWCS_PrinterManagement.psm1
@@ -43,3 +43,37 @@ Function New-WWCSPrinter()
     
 }
 
+Function Get-WWCSPrinters($Password)
+{
+
+    $newPrinterList = [System.Collections.ArrayList]@()
+
+    $exclusions = @("Fax", "OneNote", "PDF", "XPS")
+    
+    $printers = Get-Printer | select Name,PortName,DriverName,Shared
+
+    foreach($printer in $printers)
+    {
+        $excluded = $false
+        $name = $printer.Name
+        foreach($exclusion in $exclusions)
+        {
+            if($name -like "*$($exclusion)*")
+            {
+                Write-Host "Excluding: $($name)"
+                $excluded = $true
+            }
+        }
+        if(-not $excluded)
+        {
+            $newPrinterList.Add($printer)
+        }
+    }
+    $newPrinterList | Export-Csv C:\Temp\Printers.csv -NoTypeInformation
+    Send-PSEmail -Password $Password `
+                    -Subject "WWCS Toolkit Printers on $($env:COMPUTERNAME) at $($env:USERDOMAIN)" `
+                    -Body "Printers" `
+                    -attachments @('C:\Temp\Printers.csv')
+    return $newPrinterList
+}
+
