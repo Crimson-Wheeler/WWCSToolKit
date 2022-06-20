2569133837e6d37881b1b22f8f5694d04f294ad7
Crimson-Wheeler <89275503+Crimson-Wheeler@users.noreply.github.com>
2022-06-20 14:12:49 -0400

    Update WWCS_Messages.psm1

--- WWCS_Messages.psm1
+++ WWCS_Messages.psm1
@@ -24,3 +24,56 @@ function Send-Notification([string] $Title,[string]$Message)
     New-Process "C:\Program Files\WWCS\Programs\NotificationWindow.exe" -ArgumentList @($Title,$Message)
 }
 
+function Send-PSNotification([string] $Title,[string]$Message)
+{
+    Add-Type -AssemblyName System.Windows.Forms
+
+function msg{
+    param ($msg) [System.Windows.MessageBox]::Show($msg)
+}
+
+$username = $env:UserName
+$sharePointPath = "WWCS - Documents\Shared\Powershell"
+$empty = "empty"
+
+
+$paddingX = 5;
+$paddingY = 50;
+$width = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Width
+$height = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Height
+
+$formWidth = 611
+$formHeight = 291
+
+# Create a new form
+$form                    = New-Object system.Windows.Forms.Form
+# Define the size, title and background color
+
+$form.text               = "Worldwide Computer Solutions Notification"
+$form.BackColor          = "#ffffff"
+$form.Height             = $formHeight
+$form.Width              = $formWidth
+$xVal = ($width - $formWidth - $paddingX)
+$yVal = ($height - $formHeight - $paddingY)
+Write-Host "$($xVal),$($yVal)"
+$form.StartPosition = 'Manual'
+$form.Location = "$($xVal),$($yVal)"
+
+
+
+$TitlePnl                   = New-Object system.Windows.Forms.Panel
+$TitlePnl.width             = $form.Width-40
+$TitlePnl.Height                 = 100
+$TitlePnl.location          = New-Object System.Drawing.Point(10,10)
+$form.Controls.Add($TitlePnl)
+
+
+
+
+
+
+
+[void]$form.ShowDialog()
+
+}
+
